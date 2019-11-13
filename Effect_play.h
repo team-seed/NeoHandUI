55f10f6fa61fd3418aca6e694e01b6ec7dbdd43c
weiber-hsu <39873242+weiber-hsu@users.noreply.github.com>
2019-11-14 01:42:29 +0800

    new file:   Effect_play.h 	new file:   Game.h 	new file:   Object_pool.h 	modified:   Effect_play.h 	modified:   Game.h 	modified:   NeoHandUI.pro 	modified:   Object_pool.h 	modified:   main.cpp 	modified:   main.qml 	modified:   ui/option/SelectBar.qml 	modified:   ui/option/button_test.qml 	modified:   ui/option/camera_test.qml 	modified:   ui/option/mainPanel.qml 	modified:   ui/option/network_test.qml 	modified:   ui/option/param_setting.qml

---
+++ Effect_play.h
@@ -0,0 +1,33 @@
+#ifndef Effect_play_H
+#define Effect_play_H
+#include <QtConcurrent>
+#include <QThreadPool>
+#include <QSoundEffect>
+#include "object_pool.h"
+#define EFFECT_POOL_COUNT 5
+
+class Effect_play : public QObject,public QRunnable{
+    Q_OBJECT
+public:
+    Effect_play(){
+        QRunnable::setAutoDelete(false);
+        Q.setMaxThreadCount(EFFECT_POOL_COUNT);
+    }
+    void init(int object_pool_size=10,QUrl url=QUrl("")){
+        pool.init(object_pool_size,url);
+    }
+    static void fun1(QSoundEffect *s,Object_pool *p){
+        s->play();
+        p->ret_elem(s);
+    }
+    void play(){
+        QtConcurrent::run(&Q, fun1,pool.get_elem(), &pool);
+    }
+    void run(){}
+    Object_pool pool;
+    QSoundEffect *sound[EFFECT_POOL_COUNT];
+    QThreadPool Q;
+};
+#endif // Effect_play_H
+
+//QUrl::fromLocalFile(":/rythm.wav")
