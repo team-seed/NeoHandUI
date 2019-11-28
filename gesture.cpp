18c48a1309f9c3e0109f95815c15f23e8e05ac16
tratitude <tratitude@gmail.com>
2019-11-29 01:59:38 +0800

    update gesture lib

--- gesture.cpp
+++ gesture.cpp
@@ -5,60 +5,85 @@ Gesture::Gesture(){
     QObject::connect(&tracking_timer ,SIGNAL(timeout()) ,this ,SLOT(Get()) );
 
     swipe = 0.05;
-    cur_id = -1;
-    cur_x = 0;
-    cur_y = 0;
+    ges_cur = gesture_t(0.f, 0.f, -1);
+#ifdef SECOND_HAND
+    ges_cur_second = gesture_t(0.f, 0.f, -1);
+#endif
 }
 
 float Gesture::Readx(){
-    return cur_x;
+    return ges_cur.x;
 }
 
 float Gesture::Ready(){
-    return cur_y;
+    return ges_cur.y;
 }
 
 void Gesture::Get(){
     //更新
-    last_id = cur_id;
-    last_x = cur_x;
-    last_y = cur_y;
+    ges_last = ges_cur;
 
     //# Gesture Library
     //* Discription:
     //  * screen as 256*256, u-v coordinate system
     //  * x= bounding box central u, y= bounding box central v, z= gestureID
-
     //* Create gesture a object:
-    landmarks_to_shm::gesture ges;
+    landmarks_to_shm::gesture gesObj(
+        landmarks_datatype::norm_landmark_name,
+        landmarks_datatype::bbCentral_name,
+        landmarks_datatype::shm_name);
 
     //* Get share memory array rom shared library
     //  * open the managed segment
-
     boost::interprocess::managed_shared_memory segment(
-        boost::interprocess::open_or_create,
-        landmarks_datatype::shm_name,
+        boost::interprocess::open_or_create, gesObj.shm_name_,
         landmarks_datatype::shm_size);
 
     //  * find the share memory array
-    landmarks_datatype::coordinate3d_t *_bbCentral_ptr = segment.find<landmarks_datatype::coordinate3d_t>(
-        landmarks_datatype::bbCentral_name).first;
+    landmarks_datatype::coordinate3d_t *bbCentral =
+        segment.find<landmarks_datatype::coordinate3d_t>(
+        gesObj.bbCentral_shm_name_).first;
 
     //* get bounding box central u
-    float u = _bbCentral_ptr[0].x;
-
     //* get bounding box central v
-    float v = _bbCentral_ptr[0].y;
-
     //* Get gestureID
     //  * if id=-1 not a predefined gesture
     //  * else id=0~31 is a predefined gesture
-    int gestureID = (int)_bbCentral_ptr[0].z;
+    ges_cur = {bbCentral[0].x, bbCentral[0].y, (int)bbCentral[0].z};
 
-    cur_x = u;
-    cur_y = v;
-    cur_id = gestureID;
+#ifdef SECOND_HAND
+    //更新
+    ges_last_second = ges_cur_second;
 
+    //# Gesture Library
+    //* Discription:
+    //  * screen as 256*256, u-v coordinate system
+    //  * x= bounding box central u, y= bounding box central v, z= gestureID
+    //* Create gesture a object:
+    landmarks_to_shm::gesture gesObj_second(
+        landmarks_datatype::norm_landmark_name_second,
+        landmarks_datatype::bbCentral_name_second,
+        landmarks_datatype::shm_name_second);
+
+    //* Get share memory array rom shared library
+    //  * open the managed segment
+    boost::interprocess::managed_shared_memory segment_second(
+        boost::interprocess::open_or_create, gesObj_second.shm_name_,
+        landmarks_datatype::shm_size);
+
+    //  * find the share memory array
+    landmarks_datatype::coordinate3d_t *bbCentral_second =
+        segment_second.find<landmarks_datatype::coordinate3d_t>(
+        gesObj_second.bbCentral_shm_name_).first;
+
+    //* get bounding box central u
+    //* get bounding box central v
+    //* Get gestureID
+    //  * if id=-1 not a predefined gesture
+    //  * else id=0~31 is a predefined gesture
+    ges_cur_second = {bbCentral_second[0].x, bbCentral_second[0].y, (int)bbCentral_second[0].z};
+    std::cout << ges_cur_second;
+#endif
     emit Xchanged();
     emit Ychanged();
     check_type();
@@ -71,14 +96,14 @@ void Gesture::Get(){
 
 void Gesture::check_type(){
     //手勢改變
-    if (cur_id != last_id) {
+    if (ges_cur.id != ges_last.id) {
 
-        if (cur_id == -1) {
+        if (ges_cur.id == -1) {
             cur_gest = -1;
             emit untrigger();
         }
         //ges1 介於0~10
-        else if (0 <= cur_id && cur_id < 10 && cur_gest != 0) {
+        else if (0 <= ges_cur.id && ges_cur.id < 10 && cur_gest != 0) {
             cur_gest = 0;
             emit trigger();
         }
@@ -88,43 +113,49 @@ void Gesture::check_type(){
     }
 
     //手勢不變 x變 ****新增手勢要加****
-//    else if ( 0 <= cur_id && cur_id < 10)
+//    else if ( 0 <= ges_cur.id && ges_cur.id < 10)
 //        if( qAbs(cur_x-last_x) > x_shift )
 //            emit Xchanged();
 }
 
 
 void Gesture::check_movement(){
-    float tmp_x = cur_x - last_x;
-    float tmp_y = cur_y - last_y;
+    gesture_t tmp = ges_cur - ges_last;
 
     //算角度 (0~pi/2)第一 (pi/2 ~ pi)第二 (－pi～－pi/2)第三 ( -pi/2～0)第四
-    if( qSqrt(qPow(tmp_x,2) + qPow(tmp_y,2)) >= swipe ){
+    if( qSqrt(qPow(tmp.x,2) + qPow(tmp.y,2)) >= swipe ){
 
         //水平或垂直
-        if( tmp_x == 0.f ){
+        if( tmp.x == 0.f ){
             //垂直位移達標
-            if( tmp_y > 0.f ) emit down_swipe();
+            if( tmp.y > 0.f ) emit down_swipe();
             else emit up_swipe();
             return;
         }
-        else if ( tmp_y == 0.f ) {
+        else if ( tmp.y == 0.f ) {
             //水平位移達標
-            if( tmp_x > 0.f ) emit right_swipe();
+            if( tmp.x > 0.f ) emit right_swipe();
             else emit left_swipe();
             return;
         }
 
-        double angle = qRadiansToDegrees(qAtan2(qAbs(tmp_y), qAbs(tmp_x)));
+        double angle = qRadiansToDegrees(qAtan2(qAbs(tmp.y), qAbs(tmp.x)));
 
         if (angle < 50) {
-            if (tmp_x > 0) emit right_swipe();
+            if (tmp.x > 0) emit right_swipe();
             else emit left_swipe();
         }
 
         if (angle > 40) {
-            if (tmp_y > 0) emit down_swipe();
+            if (tmp.y > 0) emit down_swipe();
             else emit up_swipe();
         }
     }
 }
+
+std::ostream& operator <<(std::ostream& os, const gesture_t &ges)
+{
+    os << "gesture: " << ges.x << " " << ges.y << " " << ges.id << "\n";
+    return os;
+}
+
--- gesture.h
+++ gesture.h
@@ -6,6 +6,30 @@
 #include <QTimer>
 #include <QtMath>
 #include <QtDebug>
+
+#include <iostream>
+
+struct gesture_t{
+    float x, y;
+    int id;
+
+    gesture_t(float _x = 0.f, float _y = 0.f, int _id = -1)
+        :x(_x), y(_y), id(_id){}
+
+    gesture_t& operator =(const gesture_t &ges)
+    {
+        x = ges.x; y = ges.y; id = ges.id;
+        return *this;
+    }
+
+    gesture_t operator -(const gesture_t &ges) const
+    {
+        return gesture_t(x-ges.x, y-ges.y, id);
+    }
+};
+
+std::ostream& operator <<(std::ostream& os, const gesture_t &ges);
+
 class Gesture : public QObject
 {
     Q_OBJECT
@@ -43,14 +67,15 @@ public:
     float Ready();
 
     //數值更新寫在 GET() 開頭
-    int last_id;
-    float last_x, last_y;
-    int cur_id;
-    float cur_x, cur_y;
-
+    gesture_t ges_last;
+    gesture_t ges_cur;
     int cur_gest;
-
     float swipe;  //定義移動多少觸發swipe
+
+#ifdef SECOND_HAND
+    gesture_t ges_last_second;
+    gesture_t ges_cur_second;
+#endif
 
     QTimer tracking_timer;
 };
